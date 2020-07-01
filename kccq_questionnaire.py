81cd9c850047b8aa7be2c738977bd3bdc2ac2852
ssssssophia <wangsych@163.com>
2020-07-01 15:21:51 +0800

    update

--- kccq_questionnaire.py
+++ kccq_questionnaire.py
@@ -9,6 +9,7 @@ import datetime
 import json
 import pandas as pd
 import numpy as np
+import os
 
 from dash.dependencies import Input, Output, State
 
@@ -18,6 +19,8 @@ from app import app
 app = dash.Dash(__name__, url_base_pathname='/login/')
 
 server = app.server
+
+username = "demo-patient"
 
 q2=["2. Over the past 2 weeks, how many times did you have swelling in your feet, ankles or legs when you woke up in the morning?",
 ["Every morning",
@@ -45,10 +48,10 @@ q4=["4. Over the past 2 weeks, on average, how many times has shortness of brea
 "Never over the  past 2 weeks"]]
 
 q5=["5. Over the past 2 weeks, on average, how many times have you been forced to sleep sitting up in a chair or with at least 3 pillows to prop you up because of shortness of breath?",
-["Every night"
-"3 or more times per week but not every day"
-"1-2 times per week"
-"Less than once a week"
+["Every night",
+"3 or more times per week but not every day",
+"1-2 times per week",
+"Less than once a week",
 "Never over the  past 2 weeks"]]
 q6=["6. Over the past 2 weeks, how much has your heart failure limited your enjoyment of life?",
 ["It has extremely  limited my enjoyment of life",
@@ -64,9 +67,23 @@ q7=["7. If you had to spend the rest of your life with your heart failure the wa
 "Mostly satisfied",
 "Completely satisfied",]]
 
+def modal_kccq_questionaire(app):
+    return html.Div(
+        [
+        dbc.Button("Start", id = 'kccq-modal-button-open'),
+        dbc.Modal(
+            [
+            dbc.ModalHeader("KCCQ Questionnaire"),
+            dbc.ModalBody(modal_kccq_questionaire_body()),
+            dbc.ModalFooter(
+                dbc.Button("submit", id="kccq-modal-button-submit", className="mr-2"),
+                )],
+            id = "kccq-modal",
+            size = 'xl'
+            )]
+        )
 
-
-def create_layout(app):
+def modal_kccq_questionaire_body():
 	
 	return html.Div(
                 [
@@ -82,52 +99,80 @@ def create_layout(app):
                                     html.Div(
                                         dbc.Row(
                                             [
-                                                dbc.Col(),
-                                                dbc.Col("1"),
-                                                dbc.Col("2"),
-                                                dbc.Col("3"),
-                                                dbc.Col("4"),
-                                                dbc.Col("5"),
-                                                dbc.Col("6"),
+                                                dbc.Col(width = 2),
+                                                dbc.Col("Extremely Limited"),
+                                                dbc.Col("Quite a bit Limited"),
+                                                dbc.Col("Moderately Limited"),
+                                                dbc.Col("Slightly Limited"),
+                                                dbc.Col("Not at all Limited"),
+                                                dbc.Col("Limited for other reasons or did not do the activity"),
+                                            ],
+                                            style = {"display" : "flex", "justify-content" : "space-around", "text-align" : "center"} 
+                                        )
+                                    ),
+                                    html.Div(
+                                        dbc.Row(
+                                            [
+                                                dbc.Col("a. Showering/bathing", width = 2),
+                                                dbc.Col(
+                                                    dbc.RadioItems(
+                                                        options = [
+                                                            {"label": "", "value" : 1},
+                                                            {"label": "", "value" : 2},
+                                                            {"label": "", "value" : 3},
+                                                            {"label": "", "value" : 4},
+                                                            {"label": "", "value" : 5},
+                                                            {"label": "", "value" : 6},
+                                                            ],
+                                                        id = "kccq-modal-radio-q1a",
+                                                        inline = True,
+                                                        style = {"display" : "flex", "justify-content" : "space-around"} ),
+                                                    
+                                                    ),
                                             ]
                                         )
                                     ),
                                     html.Div(
                                         dbc.Row(
                                             [
-                                                dbc.Col("a. Showering/bathing"),
-                                                dbc.Col("1"),
-                                                dbc.Col("2"),
-                                                dbc.Col("3"),
-                                                dbc.Col("4"),
-                                                dbc.Col("5"),
-                                                dbc.Col("6"),
+                                                dbc.Col("b. Walking 1 block on level ground",width = 2),
+                                                dbc.Col(
+                                                    dbc.RadioItems(
+                                                        options = [
+                                                            {"label": "", "value" : 1},
+                                                            {"label": "", "value" : 2},
+                                                            {"label": "", "value" : 3},
+                                                            {"label": "", "value" : 4},
+                                                            {"label": "", "value" : 5},
+                                                            {"label": "", "value" : 6},
+                                                            ],
+                                                        id = "kccq-modal-radio-q1b",
+                                                        inline = True,
+                                                        style = {"display" : "flex", "justify-content" : "space-around"} ),
+                                                    
+                                                    ),
                                             ]
                                         )
                                     ),
                                     html.Div(
                                         dbc.Row(
                                             [
-                                                dbc.Col("b. Walking 1 block on level ground"),
-                                                dbc.Col("1"),
-                                                dbc.Col("2"),
-                                                dbc.Col("3"),
-                                                dbc.Col("4"),
-                                                dbc.Col("5"),
-                                                dbc.Col("6"),
-                                            ]
-                                        )
-                                    ),
-                                    html.Div(
-                                        dbc.Row(
-                                            [
-                                                dbc.Col("c. Hurrying or jogging (as if to catch a bus)"),
-                                                dbc.Col("1"),
-                                                dbc.Col("2"),
-                                                dbc.Col("3"),
-                                                dbc.Col("4"),
-                                                dbc.Col("5"),
-                                                dbc.Col("6"),
+                                                dbc.Col("c. Hurrying or jogging (as if to catch a bus)",width = 2),
+                                                dbc.Col(
+                                                    dbc.RadioItems(
+                                                        options = [
+                                                            {"label": "", "value" : 1},
+                                                            {"label": "", "value" : 2},
+                                                            {"label": "", "value" : 3},
+                                                            {"label": "", "value" : 4},
+                                                            {"label": "", "value" : 5},
+                                                            {"label": "", "value" : 6},
+                                                            ],
+                                                        id = "kccq-modal-radio-q1c",
+                                                        inline = True,
+                                                        style = {"display" : "flex", "justify-content" : "space-around"} ),
+                                                    
+                                                    ),
                                             ]
                                         )
                                     ),
@@ -135,14 +180,16 @@ def create_layout(app):
                             )
                         ]
                     ),
-                    question_group(q2[0], q2[1], "kccq-q2"),
-                    question_group(q3[0], q3[1], "kccq-q3"),
-                    question_group(q4[0], q4[1], "kccq-q4"),
-                    question_group(q5[0], q5[1], "kccq-q5"),
-                    question_group(q6[0], q6[1], "kccq-q6"),
-                    question_group(q7[0], q7[1], "kccq-q7"),
+                    question_group(q2[0], q2[1], "kccq-modal-radio-q2"),
+                    question_group(q3[0], q3[1], "kccq-modal-radio-q3"),
+                    question_group(q4[0], q4[1], "kccq-modal-radio-q4"),
+                    question_group(q5[0], q5[1], "kccq-modal-radio-q5"),
+                    question_group(q6[0], q6[1], "kccq-modal-radio-q6"),
+                    question_group(q7[0], q7[1], "kccq-modal-radio-q7"),
 
-                    dbc.Button("submit", id="example-button", className="mr-2"),
+                    
+
+                    html.Div(id = "kccq-modal-tempdata", style = {"display":"none"})
                 ],
                 # style={"margin-top":"-30rem","background-color":"transparent","text-align":"center"}
             )
@@ -153,7 +200,7 @@ def question_group(label, value_list, id):
 
     options = []
     for i in range(value_list_len):
-        options.append({"label":value_list[i], "value":i})
+        options.append({"label":value_list[i], "value":i+1})
 
     return html.Div(
             [
@@ -171,34 +218,52 @@ def question_group(label, value_list, id):
 
 
 
-app.layout = create_layout(app)
+app.layout = modal_kccq_questionaire(app)
 
+@app.callback(
+    Output("kccq-modal-tempdata", "children"),
+    [Input("kccq-modal-button-submit", "n_clicks")],
+    [State("kccq-modal-radio-q1a", "value"),
+    State("kccq-modal-radio-q1b", "value"),
+    State("kccq-modal-radio-q1c", "value"),
+    State("kccq-modal-radio-q2", "value"),
+    State("kccq-modal-radio-q3", "value"),
+    State("kccq-modal-radio-q4", "value"),
+    State("kccq-modal-radio-q5", "value"),
+    State("kccq-modal-radio-q6", "value"),
+    State("kccq-modal-radio-q7", "value")]
+    )
+def store_questionaire_answer(n, q1a, q1b, q1c, q2, q3, q4, q5, q6, q7):
+    submit_date = str(datetime.datetime.now().date())
+    answer = {"answer-date" : submit_date, 
+                "q1a" : q1a, "q1b" : q1b, "q1c" : q1c,
+                "q2" : q2,
+                "q3" : q3,
+                "q4" : q4,
+                "q5" : q5,
+                "q6" : q6,
+                "q7" : q7}
+    path = str('configure/') + username +str('/kccq_questionarie_') + submit_date + str('.json')
+    if not os.path.exists(str('configure/') + username +str('/')):
+        os.makedirs(str('configure/') + username +str('/'))
+    with open(path,'w') as outfile:
+        json.dump(answer, outfile)
+    return json.dumps(answer)
 
-@app.callback(Output('live-update-text', 'children'),
-            [
-              Input('interval-component', 'n_intervals'),
-              Input("number-input", "value")
-            ]
-            )
-def update_metrics(n, value):
-    style = style={"box-shadow":"0 4px 8px 0 rgba(0, 0, 0, 0.05), 0 6px 20px 0 rgba(0, 0, 0, 0.05)", "border":"none", "border-radius":"0.5rem"}
-
-    itemlist = []
-
-    if value:
-        value = int(value)
-        for i in range(value):
-            layout = html.Div(
-                        html.H2(str(i)),
-                        style = style
-                    )
-            itemlist.append(layout)
-
+@app.callback(
+    Output("kccq-modal", 'is_open'),
+    [Input("kccq-modal-button-open", "n_clicks"),
+    Input("kccq-modal-button-submit", "n_clicks")],
+    [State("kccq-modal", 'is_open')]
+    )
+def open_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
     else:
-        itemlist
+        return is_open
 
-    return itemlist
+
 
 
 if __name__ == "__main__":
-    app.run_server(host="127.0.0.1",debug=True,port=8053)
+    app.run_server(host="127.0.0.1",debug=True,port=8052)
